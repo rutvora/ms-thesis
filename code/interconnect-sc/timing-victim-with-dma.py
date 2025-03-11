61b19120470accb5b52b7f67966c686d9513eb63
Rut Vora <contactme@rutvora.com>
2025-03-11 10:30:35 -0700

    Clarify attacker and victim code for interconnect-sc

--- code/interconnect-sc/timing-victim-with-dma.py
+++ code/interconnect-sc/timing-victim-with-dma.py
@@ -1,12 +1,22 @@
-# SIZE = 4KB or 4MB
-res = []  # Store the observed latencies
+ATTACKER_ITERS = 1e4  # Arbitrarily large number
+ATTACKER_SIZE = 4KB  # or 4MB
 
-cudaEvent_t start, end
+VICTIM_ITERS = 1e3  # One order of magnitude less than ATTACKER_ITERS
+VICTIM_SIZE = 4KB  # or 4MB
 
-while(True):  # Until attacker stops it
-    cudaEventRecord(start)  # Store current GPU clock
-    cudaMemcpy(dest, src, SIZE)  # Copy SIZE amount of data 
-    cudaEventRecord(end)  # Store current GPU clock
-    cudaEventSynchronize(end)  # Ensure all previous CUDA calls are completed
-    res.append(stop - start)
+def attacker():
+    res = []  # Store the observed latencies
     
+    cudaEvent_t start, end
+    
+    for i in range(ATTACKER_ITERS):
+        cudaEventRecord(start)  # Store current GPU clock
+        cudaMemcpy(dest, src, ATTACKER_SIZE)  # Copy SIZE amount of data 
+        cudaEventRecord(end)  # Store current GPU clock
+        cudaEventSynchronize(end)  # Ensure all previous CUDA calls are completed
+        res.append(stop - start)
+
+def victim():
+    for i in range(VICTIM_ITERS):  # Until attacker stops it
+        cudaMemcpy(dest, src, VICTIM_SIZE)  # Copy SIZE amount of data 
+        
--- code/interconnect-sc/timing-victim-with-stores.py
+++ code/interconnect-sc/timing-victim-with-stores.py
@@ -1,18 +1,29 @@
-res = []  # Store the observed latencies
-timer = old_timer = now()
+ATTACKER_ITERS = 1e5  # Arbitrarily large number
 
-# Issue 85 stores
-# 85 is an arbitrary number larger than N = 81
-store 1
-.
-.
-.
-store 85
+VICTIM_ITERS = 1e4  # One order of magnitude less than ATTACKER_ITERS
+VICTIM_SIZE = 4KB  # or 4MB
 
-# Issue a timer, which will block until a scheduler slot opens up
-# Then fill the slot again with another store instruction
-while(True):  # Until attacker stops it
-    timer = now()  # Executed out of order as soon as the scheduler slot opens up
-    res.append(timer - old_timer)
-    old_timer = timer
-    store # Fill the scheduler slot again
+
+def attacker():
+    res = []  # Store the observed latencies
+    timer = old_timer = now()
+    
+    # Issue 85 stores
+    # 85 is an arbitrary number larger than N = 81
+    store 1
+    .
+    .
+    .
+    store 85
+    
+    # Issue a timer, which will block until a scheduler slot opens up
+    # Then fill the slot again with another store instruction
+    for i in range(ATTACKER_ITERS):  # Until attacker stops it
+        timer = now()  # Executed out of order as soon as the scheduler slot opens up
+        res.append(timer - old_timer)
+        old_timer = timer
+        store # Fill the scheduler slot again
+
+def victim():
+    for i in range(VICTIM_ITERS):
+        cudaMemcpy(VICTIM_SIZE)  # DMA transfer
